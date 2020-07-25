5c40cb29f0e243b262b7c0efaf69a97de7d646ac
Omar <omar.belkady@g.austincc.edu>
2020-07-25 13:05:51 +0100

    Added String_Func.rb file

---
+++ String_Func.rb
@@ -0,0 +1,11 @@
+String text = "Omar Belkady"
+puts(text.upcase())
+puts(text.length())
+age=26
+puts("My name is "+text+" and I am "+age.to_s+" years old")
+puts(text.include?("Omar"))#Outputs to True
+
+puts("Enter Your Name 968 6342 56837: ")
+String name = gets
+name=name.chomp()
+puts(name.upcase())#inputted string is outputted as all uppercase
