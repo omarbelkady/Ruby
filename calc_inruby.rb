215c68e933d57d400df508dcf80ee1f2023661e0
Omar <omar.belkady@g.austincc.edu>
2020-07-25 13:43:54 +0100

    Adding calc_inruby.rb

---
+++ calc_inruby.rb
@@ -0,0 +1,8 @@
+puts "Enter the first number to perform the concatenating operator"
+num1 = gets.chomp()
+puts "Enter the second number of the concatenation operation"
+num2 = gets.chomp()
+puts("5 added 2 is string addition or concatenating: "+(num1+num2))#this will print 52 because we are concatenating
+
+puts("This is addition: ")
+puts(num1.to_f+num2.to_f)#turning num1 and num2 into floating point numbers and added the two together
